93cb43bcb605f6bdf1f387e216e0907e0046eff4
heopath <m8765000@naver.com>
2026-02-23 15:51:42 +0900

    #2 Init Login Page

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <input type="text" placeholder="Email" v-model="email"/>
+        <input type="password" placeholder="Password" v-model="password" />
+        
+        <button @click="login">로그인하기</button>
+        <div>
+            계정이 없으신가요?
+            <span>가입하기</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"LoginPage",
+    data() {
+        return {
+            email: "",
+            password: "",
+        };
+    },
+    methods: {
+        login() {
+            console.log("이메일:", this.email);
+            console.log("비밀번호:", this.password);
+        }, 
+    },
+};
+</script>
+
+<style>
+
+</style>
