61e5dec3dd003d530a86953de1faccbac166190a
heopath <m8765000@naver.com>
2026-03-15 17:43:39 +0900

    #5 Router access control

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,62 @@
-import DemoPage from "@/views/DemoPage.vue";
-import LifeCycle from "@/views/LifeCycle.vue";
-import LoginPage from "@/views/LoginPage.vue";
-import SignUpPage from "@/views/SignUpPage.vue";
-import MainPage from "@/views/MainPage.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginPage from '../views/LoginPage.vue';
+import SignUpPage from '../views/SignUpPage.vue';
+import MainPage from '../views/MainPage.vue';
 
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: LoginPage,
+    beforeEnter: (to, from, next) => {
+      const token = localStorage.getItem('access_token');
+      if (token) {
+        next('/main');
+      } else {
+        next();
+      }
+    },
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUpPage,
+    beforeEnter: (to, from, next) => {
+      const token = localStorage.getItem('access_token');
+      if (token) {
+        next('/main');
+      } else {
+        next();
+      }
+    },
+  },
+  {
+    path: '/main',
+    name: 'MainPage',
+    component: MainPage,
+    beforeEnter: (to, from, next) => {
+      const token = localStorage.getItem('access_token');
+      if (token) {
+        next();
+      } else {
+        next('/');
+      }
+    },
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            component:LoginPage
-        },
-        {
-            path: '/demo',
-            component: DemoPage
-        },
-        {
-            path: '/login',
-            component:LoginPage
-        },
-        {
-            path: '/signup',
-            component:SignUpPage
-        },
-        {
-            path: '/main',
-            component: MainPage
-        },
-        {
-            path: '/life',
-            component: LifeCycle
-        }
-    ]
-})
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const validPaths = ['/', '/signup', '/main'];
+  if (validPaths.includes(to.path)) {
+    next();
+  } else {
+    next('/');
+  }
+});
 
 export default router;
