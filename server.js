2f3c83cfc658bcc162f6d2c4c34a53944e19af49
heopath <m8765000@naver.com>
2026-03-15 19:29:58 +0900

    #6 deploy

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const distPath = path.join(__dirname, 'dist');
+
+app.use(express.static(distPath));
+
+app.use((req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
+const SERVER_URL = process.env.NODE_ENV === 'production'
+? process.env.SERVER_URL
+:'http://localhost:3000/'
+
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: SERVER_URL,
     timeout: 5000,
 })
 
 const authAPi = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: SERVER_URL,
     timeout: 5000,
 })
 
